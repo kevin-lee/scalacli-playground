2c3ed0b90e7b6935c7374bda910462d18cfe04e5
Kevin Lee <kevin.code@kevinlee.io>
2025-06-20 15:57:34 +1000

    Add initial Scala CLI playground setup
    - Added  with a simple  function to test Scala code
    - Moved Scala CLI using directives to a centralized  file
    - Created  script for convenient execution of the playground

---
+++ playground.scala
@@ -0,0 +1,11 @@
+/* Using directives moved to project.scala */
+
+/* Scala CLI playground file
+ * Run this file with: ./run-playground.sh
+ */
+
+@main
+def play(): Unit = {
+  println("Hello, Scala!")
+
+}
---
+++ project.scala
@@ -0,0 +1,12 @@
+//> using scala "3.7.1"
+//> using dep "org.typelevel::cats-core:2.13.0"
+//> using dep "org.typelevel::cats-effect:3.6.1"
+//> using dep "org.jsoup:jsoup:1.20.1"
+//> using dep "org.typelevel::laika-core:1.3.2"
+//> using dep "io.kevinlee::refined4s-core:1.1.0"
+//> using dep "io.kevinlee::extras-scala-io:0.44.0"
+//> using dep "io.kevinlee::extras-string:0.44.0"
+//> using dep "io.kevinlee::extras-cats:0.44.0"
+//> using dep "io.kevinlee::extras-render:0.44.0"
+
+// This file centralizes all Scala CLI directives for the project
